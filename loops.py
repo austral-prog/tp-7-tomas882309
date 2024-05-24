2542c1593bae8e065a245c70f9b87ed8eb6a471d
Tomas Scherer <141924902+tomas882309@users.noreply.github.com>
2024-05-23 23:29:35 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,39 @@
 def index_of_by_index(word, list, index):
+    for indice, element in enumerate(list[index:]):
+        if element == word:
+            return indice + index
     return -1
-
+        
 
 def index_of_empty(list):
+    for index, element in enumerate(list):
+        if element == "":
+            return index
     return -1
+
 
 
 def index_of(word, list):
+    for index, element in enumerate(list):
+        if element == word:
+            return index
     return -1
 
 
 def put(word, list):
+    for index, element in enumerate(list):
+        if element == "":
+            list[index] = word
+            return index
     return -1
 
 
 def remove(word, list):
+    contador = 0
+    for index, element in enumerate(list):
+        if element == word:
+            list[index] = ""
+            contador += 1
+            print(list)
+    return contador
     return -1
