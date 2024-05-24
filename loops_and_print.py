6a432e699009f18c7bf5c9babb326c51c515eacd
Tomas Scherer <141924902+tomas882309@users.noreply.github.com>
2024-05-23 23:30:48 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,25 @@
 def enumerate_list(list):
-    return list
-
+    lista_1 = []
+    lista_2 = []
+    for elemento in list:
+        if elemento != "":
+            lista_1.append(elemento)
+            print(lista_1)
+    for index, element in enumerate(lista_1):
+            lista_2.append(f"{index}. {element}")
+    return lista_2
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_list(colors))
 
 def enumerate_backwards(list):
-    return list
+    lista_1 = []
+    lista_2 = []
+    for elemento in list:
+        if elemento != "":
+            lista_1.append(elemento[::-1])
+            print(lista_1)
+    for index, element in enumerate(lista_1):
+            lista_2.append(f"{index}. {element}")
+    return lista_2
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_backwards(colors))
